db39d17dee6af426c7daf3e2bf9c06b9b531020c
Martyono Sembodo <martyono.sembodo@gmail.com>
2018-09-23 10:41:36 +0700

    Updating VariableMapping class

--- src/main/java/com/idemia/tec/testbench/model/VariableMapping.java
+++ src/main/java/com/idemia/tec/testbench/model/VariableMapping.java
@@ -3,29 +3,40 @@ package com.idemia.tec.testbench.model;
 public class VariableMapping {
 
     private String mappedVariable;
+    private String mccVariable;
     private String value;
 
     public VariableMapping() {}
 
-    public VariableMapping(String mappedVariable, String advSaveVariable) {
-        this.mappedVariable = mappedVariable;
-        this.value = advSaveVariable;
-    }
+	public VariableMapping(String mappedVariable, String mccVariable, String value) {
+		super();
+		this.mappedVariable = mappedVariable;
+		this.mccVariable = mccVariable;
+		this.value = value;
+	}
 
-    public String getMappedVariable() {
-        return mappedVariable;
-    }
+	public String getMappedVariable() {
+		return mappedVariable;
+	}
 
-    public void setMappedVariable(String mappedVariable) {
-        this.mappedVariable = mappedVariable;
-    }
+	public void setMappedVariable(String mappedVariable) {
+		this.mappedVariable = mappedVariable;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getMccVariable() {
+		return mccVariable;
+	}
 
-    public void setValue(String value) {
-        this.value = value;
-    }
+	public void setMccVariable(String mccVariable) {
+		this.mccVariable = mccVariable;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
 
 }
